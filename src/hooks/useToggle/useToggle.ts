5ba1b3e2db74b544e46aede2265ad6e359fc6cc0
mickaelaustoni <mickael.austoni@gmail.com>
2024-04-16 09:56:54 +0200

    version: 1.13.5

--- src/hooks/useToggle/useToggle.ts
+++ src/hooks/useToggle/useToggle.ts
@@ -4,13 +4,12 @@ import { Dispatch, SetStateAction, useCallback, useState } from "react";
  * Use toggle hook to toggle a boolean state
  * @param initialState
  */
-const useToggle = (initialState = false): [boolean, (forceBoolean?: boolean) => void, Dispatch<SetStateAction<boolean>>] => {
+const useToggle = (initialState = false): [boolean, () => void, Dispatch<SetStateAction<boolean>>] => {
   const [state, setState] = useState(initialState);
 
-  const toggle = useCallback(
-    (forceBoolean?: boolean) => setState((prevState) => (forceBoolean === undefined ? !prevState : forceBoolean)),
-    [],
-  );
+  const toggle = useCallback(() => {
+    setState((prevState) => !prevState);
+  }, []);
 
   return [state, toggle, setState];
 };
