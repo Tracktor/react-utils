efd75939bffb7544b3c75e48e604257ce43ebb37
mickaelaustoni <mickael.austoni@gmail.com>
2024-04-16 09:39:01 +0200

    version: 1.13.4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
 # [Versions](https://github.com/Tracktor/react-utils/releases)
 
-## v1.13.3
-- **[fix]** :  return type `useToggle`
+## v1.13.4
+- **[fix]** : toggle dispatch function `useToggle`
--- src/hooks/useToggle/useToggle.ts
+++ src/hooks/useToggle/useToggle.ts
@@ -6,7 +6,11 @@ import { Dispatch, SetStateAction, useCallback, useState } from "react";
  */
 const useToggle = (initialState = false): [boolean, (forceBoolean?: boolean) => void, Dispatch<SetStateAction<boolean>>] => {
   const [state, setState] = useState(initialState);
-  const toggle = useCallback((forceBoolean?: boolean) => setState((prevState) => forceBoolean || !prevState), []);
+
+  const toggle = useCallback(
+    (forceBoolean?: boolean) => setState((prevState) => (forceBoolean === undefined ? !prevState : forceBoolean)),
+    [],
+  );
 
   return [state, toggle, setState];
 };
