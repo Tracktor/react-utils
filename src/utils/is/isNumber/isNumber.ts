8e2407a5ef80aa3d8d487590013caba427a2e5a5
kevin <kevin.graff1@gmail.com>
2025-03-04 11:25:26 +0100

    feat(isnumber): update to cast then check value given

--- src/utils/is/isNumber/isNumber.ts
+++ src/utils/is/isNumber/isNumber.ts
@@ -2,6 +2,8 @@
  * Checks if a value is a number
  * @param val
  */
-export const isNumber = (val: unknown): val is number => typeof val === "number";
-
+const isNumber = (val: unknown): val is number => {
+  const castValToNumber = Number(val);
+  return typeof val === "number" || !Number.isNaN(castValToNumber);
+};
 export default isNumber;
